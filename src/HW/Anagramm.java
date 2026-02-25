2eda0bb81e1870866e10dc66180ec257b093ad5b
Duegen <nikitadyugurov@gmail.com>
2026-02-25 21:07:53 +0200

    Homework17 v1.1

--- src/HW/Anagramm.java
+++ src/HW/Anagramm.java
@@ -19,6 +19,7 @@ public class Anagramm {
 		System.out.println(baseWord + " | " + "let" + " -> " + isAnagram(baseWord, "let"));
 		System.out.println(baseWord + " | " + "tree" + " -> " + isAnagram(baseWord, "tree"));
 		System.out.println(baseWord + " | " + "critic" + " -> " + isAnagram(baseWord, "critic"));
+		System.out.println(baseWord + " | " + "Critic" + " -> " + isAnagram(baseWord, "Critic"));
 
 		System.out.println("\n-------isAnagram returns false-------");
 		System.out.println(baseWord + " | " + "tot" + " -> " + isAnagram(baseWord,"tot"));
@@ -35,40 +36,33 @@ public class Anagramm {
 		if(word.isEmpty() || anagram.isEmpty()) throw new IllegalArgumentException("Base word or anagram can't be empty");
 		
 		// Converting string to Map, key - character from the input string, value - number of entries of character in the input string
-		Function<String, Map<Character, Integer>> stringToMap = new Function<String, Map<Character,Integer>>() {	
-			@Override
-			public Map<Character, Integer> apply(String t) {
-				Map<Character, Integer> result = new HashMap<>();
-				for (char ch : t.toCharArray()) {
-					Integer prev = result.get(ch);
-					if(Objects.isNull(prev)) result.put(ch, 1); 
-					else result.put(ch, 1 + prev);
-				}
-				return result;
+		
+		Function<String, Map<Character, Integer>> stringToMap = t -> {
+			Map<Character, Integer> result = new HashMap<>();
+			for (char ch : t.toCharArray()) {
+				Integer prev = result.get(ch);
+				if(Objects.isNull(prev)) result.put(ch, 1); 
+				else result.put(ch, 1 + prev);
 			}
+			return result;
 		};
 		
 		// Strings 'word' and 'anagram' are converted to maps
-		Map<Character, Integer> wordMap = stringToMap.apply(word);
-		Map<Character, Integer> anagramMap = stringToMap.apply(anagram);
+		Map<Character, Integer> wordMap = stringToMap.apply(word.toLowerCase());
+		Map<Character, Integer> anagramMap = stringToMap.apply(anagram.toLowerCase());
 		
 		// Comparing two maps, first arg - 'word' converted to map, second arg - 'anagram' converted to map
 		BiPredicate<Map<Character, Integer>, Map<Character, Integer>> mapsCompare = 
-				new BiPredicate<Map<Character, Integer>, Map<Character, Integer>>(){
-			@Override
-			public boolean test(Map<Character, Integer> baseWord, Map<Character, Integer> anagram) {
-				// If number of unique symbols in 'word' less then number of unique symbols in 'anagram' - returns false
-				if(baseWord.size() < anagram.size()) return false;
-				for(Map.Entry<Character, Integer> entry: anagram.entrySet()) {
+				(baseWordM, anagramM) -> {
+				if(baseWordM.size() < anagramM.size()) return false;
+				for(Map.Entry<Character, Integer> entry: anagramM.entrySet()) {
 					Character key = entry.getKey();
 					Integer valueInAnagram = entry.getValue();
-					Integer valueInWord = baseWord.get(key);
+					Integer valueInWord = baseWordM.get(key);
 					// If character from 'anagram' doesn't presented in 'word' or used more times then used in 'word' - returns false 
 					if(Objects.isNull(valueInWord) || valueInWord < valueInAnagram) return false;
 				}
 				return true;
-			}
-			
 		};
 		
 		// returns result of map comparison
